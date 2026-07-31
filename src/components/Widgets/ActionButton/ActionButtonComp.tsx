b7bbf15e3ef4c1f5569d05d917cb9445bd4ca880
AndreFavotto <andrefavotto@hotmail.com>
2026-07-31 16:04:43 +0200

    Add padding to buttons, change LabelFromPV default
    - Buttons content were too close to the edges - add a small padding.
    - Start with labelFromPV false, override to true for SelectionBox.

--- src/components/Widgets/ActionButton/ActionButtonComp.tsx
+++ src/components/Widgets/ActionButton/ActionButtonComp.tsx
@@ -56,7 +56,7 @@ const ActionButtonComp: React.FC<WidgetUpdate> = ({ data }) => {
           textTransform: "none",
           minWidth: 0,
           minHeight: 0,
-          padding: 0,
+          padding: 0.5,
           pointerEvents: inEditMode ? "none" : "auto",
           overflow: "hidden",
         }}
